0d8b39946d65c314eee019fbf2ef61f52d53279a
Safi <Nurzai.safi@gmail.com>
2021-11-12 16:02:56 +0300

    FEAT: new changes

---
+++ src/app.js
@@ -0,0 +1,26 @@
+// Book constructor
+function book(title, author, isbn) {
+  this.title = title;
+  this.author = author;
+  this.isbn = isbn
+}
+// UI constructors
+
+function UI()
+
+
+// Declare varivables
+const author = document.querySelector('#title').value;
+      title = document.querySelector('#title').value;
+isbn = document.querySelector('#isbn').value;
+
+document.getElementById('form-book').addEventListener('submit', function (e) {
+
+  e.preventDefault();
+
+  console.log('test');
+})
+
+function createLibrary() {
+
+}
