80aebff6bfb1a17eabc44f1a307e45f4f8cf44ee
Safi <Nurzai.safi@gmail.com>
2021-11-13 19:27:48 +0300

    FEAT: Add book to the list is done

--- src/app.js
+++ src/app.js
@@ -1,26 +1,46 @@
 // Book constructor
-function book(title, author, isbn) {
+function Book(title, author, isbn) {
   this.title = title;
   this.author = author;
-  this.isbn = isbn
+  this.isbn = isbn;
 }
 // UI constructors
 
-function UI()
+function UI(){}
 
+UI.prototype.addBookToList = function(book) {
+  const list = document.querySelector('#book-list');
+  // create Element
+  const row = document.createElement('tr');
+  row.innerHTML = `
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
+    <td><a href="#" class="delete">X</a></td>
+  `;
 
-// Declare varivables
-const author = document.querySelector('#title').value;
-      title = document.querySelector('#title').value;
-isbn = document.querySelector('#isbn').value;
+  list.appendChild(row)
+}
 
-document.getElementById('form-book').addEventListener('submit', function (e) {
+UI.prototype.clearFields = function() {
+  author = document.querySelector('#title').value = '',
+  title = document.querySelector('#author').value = '',
+  isbn = document.querySelector('#isbn').value = '';
+}
+
+document.getElementById('book-form').addEventListener('submit', function(e) {
+  // Declare varivables
+  const author = document.querySelector('#title').value,
+        title = document.querySelector('#author').value,
+        isbn = document.querySelector('#isbn').value;
+// insantatiate book
+  const book = new Book(title, author, isbn);
+  // instantiat ui
+  const ui = new UI();
+
+  ui.addBookToList(book);
+
+  ui.clearFields();
 
   e.preventDefault();
-
-  console.log('test');
-})
-
-function createLibrary() {
-
-}
+});
